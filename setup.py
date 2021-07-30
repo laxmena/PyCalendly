41beac45ba6819f742db7de601a49e0b6f54f8f8
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2021-07-29 23:07:43 -0500

    add requirements, version, keywords in setup

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup
 
 long_desc = open("README.md").read() + "\n\n" + open("HISTORY.md").read()
-required = []
+required = ['requests']
 
 setup(
     name='PyCalendly',
+    version="0.0.1",
     maintainer="laxmena",
     maintainer_email="ConnectWith@laxmena.com",
     license="MIT",
     url="https://github.com/laxmena/PyCalendly",
     download_url="https://github.com/laxmena/PyCalendly",
     long_description=long_desc,
+    long_description_content_type="text/markdown",
     version='1.0.0',
     description="Python wrapper for Calendly API v2 (https://calendly.stoplight.io/docs/api-docs/docs/C-API-Conventions.md)",
     include_package_data=True,
     zip_safe=False,
     install_requires=required,
     platforms="any",
+    keywords="Calendly python api v2",
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development",
